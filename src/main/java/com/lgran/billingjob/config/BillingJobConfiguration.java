275b585eb12974ba1f50d0b358d32d0e6f843fee
Leonid_Granatov <leonid.granatov@ihsmarkit.com>
2024-10-07 12:26:02 +0300

    DB created and config

---
+++ src/main/java/com/lgran/billingjob/config/BillingJobConfiguration.java
@@ -0,0 +1,8 @@
+package com.lgran.billingjob.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BillingJobConfiguration {
+  // TODO add job definition here
+}
