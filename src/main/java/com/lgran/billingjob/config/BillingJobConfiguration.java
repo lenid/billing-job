9a792838acce896732401579c3e30b8ff281cf73
Leonid_Granatov <leonid.granatov@ihsmarkit.com>
2024-10-08 10:59:40 +0300

    First job success

--- src/main/java/com/lgran/billingjob/config/BillingJobConfiguration.java
+++ src/main/java/com/lgran/billingjob/config/BillingJobConfiguration.java
@@ -1,8 +1,18 @@
 package com.lgran.billingjob.config;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.lgran.billingjob.BillingJob;
 
 @Configuration
 public class BillingJobConfiguration {
-  // TODO add job definition here
+
+  @Bean
+  public Job job(JobRepository jobRepository) {
+    return new BillingJob(jobRepository);
+  }
+
 }
--- src/test/java/com/lgran/billingjob/BillingJobApplicationTests.java
+++ src/test/java/com/lgran/billingjob/BillingJobApplicationTests.java
@@ -3,10 +3,10 @@ package com.lgran.billingjob;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class BillingJobApplicationTests {
 
-	@Test
+//	@Test
 	void contextLoads() {
 	}
 
